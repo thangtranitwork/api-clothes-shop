abd2277bcde063b1b0196155cf0a634c13566a94
THANG <thangtran.it.work@gmail.com>
2024-12-07 09:12:19 +0700

    REMOVE: remove Luan's code :v

--- src/main/java/com/clothes/admin/service/OrderItemAdminService.java
+++
@@ -1,9 +0,0 @@
-package com.clothes.admin.service;
-
-import com.clothes.noc.entity.OrderItem;
-
-import java.util.List;
-
-public interface OrderItemAdminService {
-    List<OrderItem> findByOrderId(String orderId);
-}
